cc4d24a0f012a23d3b2566d356d4567b842c1548
Brett Schuchert <Brett. Schuchert@gmail.com>
2018-09-27 19:06:50 -0400

    Added several operators and a few tests for an empty calculator

--- src/main/java/shoe/RpnCalculator.java
+++ src/main/java/shoe/RpnCalculator.java
@@ -1,9 +1,68 @@
 package shoe;
 
+import ch.obermuhlner.math.big.BigDecimalMath;
+
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Stack;
 
 public class RpnCalculator {
+
+    public static final MathContext UNLIMITED = MathContext.UNLIMITED;
+    private Stack<BigDecimal> values;
+
+    public RpnCalculator() {
+        values = new Stack<>();
+    }
+
     public BigDecimal x() {
-        return BigDecimal.ZERO;
+        return peek();
+    }
+
+    public void enter(BigDecimal value) {
+        values.push(value);
+    }
+
+    public void add() {
+        BigDecimal rhs = pop();
+        BigDecimal lhs = pop();
+        values.push(lhs.add(rhs));
+    }
+
+    public void subtract() {
+        BigDecimal rhs = pop();
+        BigDecimal lhs = pop();
+        values.push(lhs.subtract(rhs));
+    }
+
+    public void pow() {
+        BigDecimal rhs = pop();
+        BigDecimal lhs = pop();
+        values.push(BigDecimalMath.pow(lhs, rhs, UNLIMITED));
+    }
+
+    public void multiply() {
+        BigDecimal rhs = pop();
+        BigDecimal lhs = pop();
+        values.push(lhs.multiply(rhs));
+    }
+
+    public void divide() {
+        BigDecimal rhs = pop();
+        BigDecimal lhs = pop();
+        values.push(lhs.divide(rhs));
+    }
+
+    public void factorial() {
+        BigDecimal value = pop();
+        values.push(BigDecimalMath.factorial(value, MathContext.UNLIMITED));
+    }
+
+    BigDecimal peek() {
+        return values.size() > 0 ? values.peek() : BigDecimal.ZERO;
+    }
+
+    BigDecimal pop() {
+        return values.size() > 0 ? values.pop() : BigDecimal.ZERO;
     }
 }
--- src/test/java/shoe/RpnCalculatorTest.java
+++ src/test/java/shoe/RpnCalculatorTest.java
@@ -1,15 +1,99 @@
 package shoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RpnCalculatorTest {
+
+    private RpnCalculator calculator;
+
+    @BeforeEach
+    void setup() {
+        calculator = new RpnCalculator();
+    }
+
     @Test
     void initialValueZero() {
-        RpnCalculator calculator = new RpnCalculator();
         assertEquals(BigDecimal.ZERO, calculator.x());
+    }
+
+    @Test
+    void addingTwoNumers() {
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.add();
+        assertEquals(BigDecimal.valueOf(8), calculator.x());
+    }
+
+    @Test
+    void subtractingTwoNumers() {
+        calculator.enter(BigDecimal.valueOf(2));
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.subtract();
+        assertEquals(BigDecimal.valueOf(-2), calculator.x());
+    }
+
+    @Test
+    void addThenSubtract() {
+        calculator.enter(BigDecimal.valueOf(11));
+        calculator.enter(BigDecimal.valueOf(2));
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.add();
+        calculator.subtract();
+        assertEquals(BigDecimal.valueOf(5), calculator.x());
+    }
+
+    @Test
+    void multiplyingTwoNumbers() {
+        calculator.enter(BigDecimal.valueOf(2));
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.multiply();
+        assertEquals(BigDecimal.valueOf(8), calculator.x());
+    }
+
+    @Test
+    void divideTwoNumbers() {
+        calculator.enter(BigDecimal.valueOf(2));
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.divide();
+        assertEquals(BigDecimal.valueOf(.5), calculator.x());
+    }
+
+    @Test
+    void pow() {
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.enter(BigDecimal.valueOf(3));
+        calculator.pow();
+        assertEquals(BigDecimal.valueOf(64), calculator.x());
+    }
+
+    @Test
+    void factorial() {
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.factorial();
+        assertEquals(BigDecimal.valueOf(24), calculator.x());
+    }
+
+    @Test
+    void addingWithNoNumbersEnteredIs0() {
+        calculator.add();
+        assertEquals(BigDecimal.ZERO, calculator.x());
+    }
+
+    @Test
+    void subtractionWithOneValueNegatesValue() {
+        calculator.enter(BigDecimal.valueOf(4));
+        calculator.subtract();
+        assertEquals(BigDecimal.valueOf(-4), calculator.x());
+    }
+
+    @Test
+    public void divideByZeroWhenCalculatorEmpty() {
+        assertThrows(ArithmeticException.class, calculator::divide);
     }
 }
