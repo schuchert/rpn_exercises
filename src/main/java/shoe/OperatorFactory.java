3ad83aa57624925b7d385c0c3d3e5eec59f1723d
Brett Schuchert <Brett. Schuchert@gmail.com>
2018-09-28 00:30:15 -0400

    Clever factory implementation that dynamically loads all of the operators and registers them. Also checks that there's at least a test for each one.

--- src/main/java/shoe/OperatorFactory.java
+++ src/main/java/shoe/OperatorFactory.java
@@ -1,22 +1,76 @@
 package shoe;
 
-public class OperatorFactory {
-    public Operator operatorFor(String operatorName) {
-        switch (operatorName) {
-            case "add":
-                return new Add();
-            case "subtract":
-                return new Subtract();
-            case "multiply":
-                return new Multiply();
-            case "divide":
-                return new Divide();
-            case "pow":
-                return new Pow();
-            case "factorial":
-                return new Factorial();
-        }
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ScanResult;
 
-        throw new IllegalArgumentException(String.format("Operator: '%s' does not exist", operatorName));
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class OperatorFactory {
+    private Map<String, Operator> operators;
+
+    public OperatorFactory() {
+        operators = new HashMap<>();
+        loadInto(operators);
+    }
+
+    public Operator operatorFor(String operatorName) {
+        Operator op = operators.get(operatorName);
+        if (op == null)
+            throw new IllegalArgumentException(String.format("Operator: '%s' does not exist", operatorName));
+        return op;
+    }
+
+    private void loadInto(Map<String, Operator> operators) {
+        try (ScanResult result = scanClasses()) {
+            operatorConstructors(result).forEach(c -> operators.put(c.getDeclaringClass().getSimpleName().toLowerCase(), oneOf(c)));
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Operator oneOf(Constructor<Operator> c) {
+        try {
+            return c.newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ScanResult scanClasses() {
+        return new ClassGraph().whitelistPackages("shoe").removeTemporaryFilesAfterScan().scan();
+    }
+
+    private List<Constructor<Operator>> operatorConstructors(ScanResult scanResult) {
+        return scanResult
+                .getClassesImplementing(Operator.class.getCanonicalName())
+                .loadClasses()
+                .stream()
+                .filter(c -> !shouldSkip(c))
+                .map(this::constructorFor)
+                .collect(Collectors.toList());
+    }
+
+    private Constructor<Operator> constructorFor(Class<?> c) {
+        try {
+            @SuppressWarnings("unchecked") final Constructor<Operator> constructor = (Constructor<Operator>) c.getConstructor();
+            return constructor;
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private boolean shouldSkip(Class<?> c) {
+        return c.getConstructors()[0].getParameterCount() > 0 || Modifier.isAbstract(c.getModifiers());
+    }
+
+    public int count() {
+        return operators.size();
     }
 }
