a01d878b7f70d2bb95606dd60cab6924f66d1777
Brett Schuchert <Brett. Schuchert@gmail.com>
2018-09-28 16:30:53 -0400

    First binary operator implemented

--- src/main/java/shoe/Add.java
+++ src/main/java/shoe/Add.java
@@ -2,11 +2,9 @@ package shoe;
 
 import java.math.BigDecimal;
 
-public class Add implements Operator {
+public class Add extends BinaryOperator {
     @Override
-    public void execute(RpnStack values) {
-        BigDecimal rhs = values.pop();
-        BigDecimal lhs = values.pop();
-        values.push(lhs.add(rhs));
+    BigDecimal calculate(BigDecimal lhs, BigDecimal rhs) {
+        return lhs.add(rhs);
     }
 }
